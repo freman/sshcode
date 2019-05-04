7d3e804dbf659d2aa3e0d3da039792fa87d48e08
Shannon Wynter <freman@users.noreply.github.com>
2019-05-04 17:39:40 +1000

    windows now known hosts

--- knownhosts_windows.go
+++ knownhosts_windows.go
@@ -1,23 +1,10 @@
 package main
 
 import (
+	"github.com/freman/putty_hosts"
 	"golang.org/x/crypto/ssh"
 )
 
 func KnownHostsHandler() ssh.HostKeyCallback {
-	// todo
-	// return func(hostname string, remote net.Addr, key ssh.PublicKey) error {
-	// 	k, err := registry.OpenKey(registry.CURRENT_USER, `Software\SimonTatham\PuTTY\SshHostKeys`, registry.QUERY_VALUE)
-	// 	if err != nil {
-	// 		log.Fatal(err)
-	// 	}
-	// 	defer k.Close()
-
-	// 	s, _, err := k.GetStringValue("SystemRoot")
-	// 	if err != nil {
-	// 		log.Fatal(err)
-	// 	}
-	// 	fmt.Printf("Windows system root is %q\n", s)
-	// }
-	return ssh.InsecureIgnoreHostKey()
+	return putty_hosts.KnownHosts()
 }
